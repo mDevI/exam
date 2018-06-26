aa9e6f9b7c765f8d7d87d5d6e1b3bf1ed451a8d7
Sergei.Belonosov <2belonosov@gmail.com>
2018-06-26 22:36:21 +0700

    FIX: Logger has been removed out in main class ExamSystem

--- src/main/java/com/mdevi/exam/ExamSystem.java
+++ src/main/java/com/mdevi/exam/ExamSystem.java
@@ -1,8 +1,6 @@
 package com.mdevi.exam;
 
 import com.mdevi.exam.service.ExamProcess;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
